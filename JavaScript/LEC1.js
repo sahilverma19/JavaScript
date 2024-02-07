ef1d425eae5f789e92464a8d042e5ac599c3297c
sahilverma19 <158835394+sahilverma19@users.noreply.github.com>
2024-02-07 19:35:00 +0000

    new

---
+++ JavaScript/LEC1.js
@@ -0,0 +1,8 @@
+const accountId=1000
+let accoutEmail="sahilverma19122@gmail.accoutEmail"
+let accountPassword="1918912"
+
+accountId=1001
+accountEmail="sahilverma220@gmail.com"
+accountPassword="@viratkohli18abd17"
+console.table([accountId,accountPassword,accoutEmail] );
