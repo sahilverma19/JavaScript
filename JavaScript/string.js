12d41e3cdfc920f17dc5a8d0d2429c329d021d1e
sahilverma19 <158835394+sahilverma19@users.noreply.github.com>
2024-02-09 18:14:24 +0000

    yo

--- JavaScript/string.js
+++
@@ -1,2 +0,0 @@
-let string="sahilverma"
-console.log(string.length);
---
+++ JavaScript/string5.js
@@ -0,0 +1,13 @@
+let string="sahilverma"
+console.log(string.length);
+console.log(string.toUpperCase());
+console.log(string.replace("verma","e"));
+
+
+
+
+
+
+
+
+
