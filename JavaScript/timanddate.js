edcfd08aa3d9c0c05a0ca0f2f4cfa1e3b7c29ec1
sahilverma19 <158835394+sahilverma19@users.noreply.github.com>
2024-02-09 20:11:27 +0000

    yo

--- JavaScript/timanddate.js
+++ JavaScript/timanddate.js
@@ -1,2 +1,6 @@
 const mydate=new Date()
-console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.getDate());
+
+const yourdate=new Date(20,10,2024,1,34,)
+console.log(yourdate.toDateString());
