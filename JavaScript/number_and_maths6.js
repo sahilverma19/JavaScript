35e121a60d6cd9657cdfd3dec0e645e0e8b2410c
sahilverma19 <158835394+sahilverma19@users.noreply.github.com>
2024-02-09 19:48:50 +0000

    YO

--- JavaScript/number_and_maths6.js
+++ JavaScript/number_and_maths6.js
@@ -1,2 +1,19 @@
-const number=1000
-console.log(number.toFixed(3));
+// const number=1000
+// console.log(number.toFixed(3));
+
+// const amount=new Number(1234.1234)
+
+// console.log(amount.toString().charAt(2));/*change in string for only this line*/
+// console.log(typeof amount)
+// console.log(amount.toPrecision(6));
+// console.log(amount.toLocaleString('en-IN'));
+// /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ maths+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ 
+console.log(Math);
+console.log(Math.abs(-1000));
+console.log(Math.max(10,33,23,34,45,56,67,87,500,699,478,1002));
+console.log(Math.random()*10+1);
+
+let min=10;
+let max=20;
+console.log(Math.floor(Math.random()*((max-min)+1)+min));
