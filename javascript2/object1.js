10886b5ace1ed696996452bf8e3cb506a0600394
sahilverma19 <158835394+sahilverma19@users.noreply.github.com>
2024-02-10 22:23:32 +0000

    yo

--- javascript2/object1.js
+++ javascript2/object1.js
@@ -1,4 +1,4 @@
-const mysym=Symbol(key1)
+const mysym=Symbol("key1")
 const object={
     name:"sahilverma",
     college:"rcetBhilai",
@@ -9,7 +9,19 @@ const object={
     [mysym]:"mykey1" 
 
 }
-console.log(object);
+//console.log(object);
 console.log(object.college);
 console.log(object["name"],object["college"]);
 console.log(typeof object.mysym);
+object.age="19";
+//console.log(object);
+//Object.freeze(object)
+object.college="sankaracharya";
+console.log(object);
+
+object.greeting=function()
+{
+    console.log("hii my name is sahil verma");
+}
+console.log(object.greeting());
+
